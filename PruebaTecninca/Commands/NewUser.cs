bfc06eb93a554e5e0e8704018074dd5f64e707e4
Tino <142237582+tinon0@users.noreply.github.com>
2024-09-26 16:26:11 -0300

    Delete

--- PruebaTecninca/Commands/NewUser.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PruebaTecninca.Commands
-{
-    public class NewUser
-    {
-        public string Name { get; set; }
-        public string Mail { get; set; }
-        public string Password { get; set; }
-    }
-}
--- PruebaTecninca/DTO/UserDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PruebaTecninca.DTO
-{
-    public class UserDTO
-    {
-        public string Name { get; set; }
-        public string Mail { get; set; }
-        public string Password { get; set; }
-    }
-}
